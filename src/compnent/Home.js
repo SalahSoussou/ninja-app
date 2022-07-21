c4f111359f3c067a272bad6b6ce61087d84e47e3
Salah Eddine Soussou <88331708+SalahSoussou@users.noreply.github.com>
2022-07-21 21:07:28 +0100

    ok

--- src/compnent/Home.js
+++ src/compnent/Home.js
@@ -1,7 +1,19 @@
 import React from "react";
 
 const Home = () => {
-  return <div className="home">Home</div>;
+  const [num, setnum] = React.useState(1);
+
+  const handleClick = () => {
+    setnum(num + 1);
+  };
+
+  return (
+    <div className="home">
+      <h2>Home page</h2>
+      <h1>{num}</h1>
+      <button onClick={handleClick}>Click me</button>
+    </div>
+  );
 };
 
 export default Home;
