bfef9ac01c6e2ded0f6f6aa0f48ffec8c7e394cc
Salah Eddine Soussou <88331708+SalahSoussou@users.noreply.github.com>
2022-07-22 11:50:42 +0100

    fin

--- src/compnent/Home.js
+++ src/compnent/Home.js
@@ -2,20 +2,29 @@ import { useState, useEffect } from "react";
 import Blogs from "../BlogList";
 
 const Home = () => {
-  const [blogs, setblogs] = useState(null);
+  const [blogs, setblogs] = useState(null),
+    [isloding, setLoding] = useState(true);
 
   useEffect(() => {
-    fetch("http://localhost:8000/blogs")
-      .then((res) => {
-        return res.json();
-      })
-      .then((data) => {
-        setblogs(data);
-      });
+    setTimeout(() => {
+      fetch("http://localhost:8000/blogs")
+        .then((res) => {
+          return res.json();
+        })
+        .then((data) => {
+          setblogs(data);
+          setLoding(false);
+        });
+    }, 5000);
   }, []);
 
   return (
     <div className="home">
+      {isloding && (
+        <div>
+          <h1>loading...</h1>
+        </div>
+      )}
       {blogs && <Blogs blogs={blogs} title="All Blogs!" />}
     </div>
   );
