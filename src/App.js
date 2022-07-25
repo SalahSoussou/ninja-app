0d8e27b943e44fb61afa3478cd38520ac1bc438d
Salah Eddine Soussou <88331708+SalahSoussou@users.noreply.github.com>
2022-07-25 18:24:50 +0100

    ok

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./compnent/Home";
 import Nav from "./compnent/Nav";
 
 function App() {
   return (
-    <div className="App">
-      <Nav />
-      <div className="content">
-        <Home />
+    <Router>
+      <div className="App">
+        <Nav />
+        <div className="content">
+          <Routes>
+            <Route path="/" element={<Home />}></Route>
+          </Routes>
+        </div>
       </div>
-    </div>
+    </Router>
   );
 }
 
