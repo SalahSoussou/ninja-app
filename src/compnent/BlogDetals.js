40a851fffc791d1ccb34ad460f6d04aa6490222b
Salah Eddine Soussou <88331708+SalahSoussou@users.noreply.github.com>
2022-07-26 22:05:36 +0100

    blog

--- src/compnent/BlogDetals.js
+++ src/compnent/BlogDetals.js
@@ -1,13 +1,20 @@
 import React from "react";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import useFetch from "./useFetch";
 
 function BlogDetals() {
   const { id } = useParams();
+  const hestory = useNavigate();
   const { data, isloding, error } = useFetch(
     "http://localhost:8000/blogs/" + id
   );
-
+  const handleDilet = () => {
+    fetch("http://localhost:8000/blogs/" + id, {
+      method: "DELETE",
+    }).then(() => {
+      hestory("/");
+    });
+  };
   return (
     <div className="blog-details">
       {isloding && <h1>Loding...</h1>}
@@ -17,6 +24,7 @@ function BlogDetals() {
           <h2> {data.title}</h2>
           <p>By {data.author}</p>
           <div>{data.body}</div>
+          <button onClick={handleDilet}>delete</button>
         </article>
       )}
     </div>
